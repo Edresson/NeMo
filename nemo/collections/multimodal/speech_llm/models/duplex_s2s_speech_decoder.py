e51e73526a108e17db7154d1f505acad546c2127
Edresson Casanova <edresson1@gmail.com>
2025-04-23 13:49:43 -0700

    Fix loss mask and add dynamic_text_non_sil_and_bos_eos

--- nemo/collections/multimodal/speech_llm/models/duplex_s2s_speech_decoder.py
+++ nemo/collections/multimodal/speech_llm/models/duplex_s2s_speech_decoder.py
@@ -2008,6 +2008,7 @@ class S2sModularAudioGPTModelSpeechDecoder(ModularAudioGPTModel):
         new_user_signal_length = audio_batch['audio_signal_length']
         new_agent_signal = audio_batch['answer_audio']
         new_agent_signal_length = audio_batch['answer_audio_lens']
+        answer_audio_lens = audio_batch['answer_audio_lens']
         loss_mask = None
         duplex_method = self.cfg.duplex_method
         assert duplex_method == "from_duplex"
@@ -2138,6 +2139,17 @@ class S2sModularAudioGPTModelSpeechDecoder(ModularAudioGPTModel):
         input_text_tokens = input_text_tokens[:, : encoded.shape[1]]
 
         loss_mask = torch.ones_like(labels)
+
+        # set the mask based on the audio lens to disconsider sequence padding in loss
+        for i in range(answer_audio_lens.size(0)):
+            speech_end_idx = math.ceil(
+                answer_audio_lens[i] / self.codec_model_downsampling_factor / self.cfg.get("decoder_reduction_factor", 1)
+            )
+            loss_mask[i, speech_end_idx:, :] = 0
+
+        # define here the speech_mask
+        speech_mask = loss_mask[:, :, -1].reshape(loss_mask.size(0), loss_mask.size(1)).clone()
+
         assert self.cfg.get(
             'duplex_loss_on_all_steps', False
         ), "only support duplex_loss_on_all_steps in real duplex data read from dataloader"
@@ -2171,15 +2183,42 @@ class S2sModularAudioGPTModelSpeechDecoder(ModularAudioGPTModel):
                     sliced_text_channel = text_channel[: loss_mask.shape[1]].unsqueeze(-1)
                     loss_mask = torch.where(labels[:, :, :] != labels[:, :1, :], 4.0, loss_mask)
                 else:
-                    raise ValueError("scale_loss_mask_by=bos_eos is only supported for target_texts_merge")
+                    raise ValueError("scale_loss_mask_by=non_sil is only supported for target_texts_merge")
         elif scale_loss_mask_by == 'non_sil_st':
             if 'target_texts_merge' in audio_batch:
                 loss_mask = torch.where(labels[:, :, :1] != labels[i, :1, :1], 4.0, loss_mask)
             else:
-                raise ValueError("scale_loss_mask_by=bos_eos is only supported for target_texts_merge")
+                raise ValueError("scale_loss_mask_by=non_sil_st is only supported for target_texts_merge")
         elif scale_loss_mask_by == 'non_sil_t':
             if 'target_texts_merge' in audio_batch:
                 loss_mask[:, :, :1] = torch.where(labels[:, :, :1] != labels[i, :1, :1], 4.0, loss_mask[:, :, :1])
+            else:
+                raise ValueError("scale_loss_mask_by=non_sil_t is only supported for target_texts_merge")
+        
+        elif scale_loss_mask_by == 'dynamic_text_non_sil_and_bos_eos':
+            if 'target_texts_merge' in audio_batch:
+                text_silence_token_id = self.tokenizer.pad_id if hasattr(self.tokenizer, 'pad_id') and self.tokenizer.pad_id >= 0 else self.tokenizer.unk_id
+                
+                # Set text loss weights
+                for i, answer_codec in enumerate(answer_codecs):
+                    current_mask = loss_mask[i, :, :1]
+                    num_real_padding_tokens = (torch.numel(current_mask) - current_mask.sum()).item()
+                    silence_idxs = labels[i, :, :1] == text_silence_token_id
+                    # ignore the padding ids
+                    silence_idxs = silence_idxs * current_mask.bool()
+                    num_silence_tokens = silence_idxs.sum().item()
+                    num_non_silence = torch.numel(silence_idxs) - num_real_padding_tokens
+                    factor = num_silence_tokens/num_non_silence
+                    
+                    # make silence text tokens 2 x times less relevant in the loss than the silence tokens
+                    new_weight = factor / 2
+                    loss_mask[i, :, :1] = torch.where(silence_idxs, new_weight, loss_mask[i, :, :1])
+
+                # set eos/bos 6x more important than a speech tokens and 12x more than a silence, this is that high because we will have only one bos/eos per turn and if it is nor right predicted the model will not produce text/speech
+                text_channel = audio_batch['target_texts_merge'][i]
+                sliced_text_channel = text_channel[: loss_mask.shape[1]].unsqueeze(-1)
+                loss_mask[:, :, :1] = torch.where(sliced_text_channel == self.tokenizer.bos_id, 6.0, loss_mask[:, :, :1])
+                loss_mask[:, :, :1] = torch.where(sliced_text_channel == self.tokenizer.eos_id, 6.0, loss_mask[:, :, :1])
             else:
                 raise ValueError("scale_loss_mask_by=bos_eos is only supported for target_texts_merge")
 
@@ -2208,7 +2247,7 @@ class S2sModularAudioGPTModelSpeechDecoder(ModularAudioGPTModel):
         attention_mask = self._create_attention_mask(encoder_input)
         if not hasattr(lm_embedding, 'transpose_batch_sequence') or lm_embedding.transpose_batch_sequence:
             encoder_input = encoder_input.transpose(0, 1).contiguous()
-        return encoder_input, attention_mask, labels, loss_mask, (encoded, encoder_length, input_audio_tokens, input_text_tokens, speaker_encoder_emb)
+        return encoder_input, attention_mask, labels, loss_mask, (encoded, encoder_length, input_audio_tokens, input_text_tokens, speaker_encoder_emb, speech_mask)
 
     def inject_speaker_prompt(self, audio_batch, encoder_input, labels, loss_mask, encoded, encoder_length):
         fixed_speaker_prompt = self.cfg.get('fixed_speaker_prompt', False)
@@ -2492,13 +2531,13 @@ class S2sModularAudioGPTModelSpeechDecoder(ModularAudioGPTModel):
             input_text_tokens = extra_inputs[3]
             speech_encoder_emb = extra_inputs[0]
             speaker_encoder_emb = extra_inputs[4]
+            speech_mask = extra_inputs[5]
 
             # make sure that the input is detached
             if self.cfg.get('freeze_all_except_speech_decoder', False) or self.model.train_only_speech_decoder:
                 self.model.speech_decoder.detach_input = True
 
             # use last position of loss mask as speech mask
-            speech_mask = loss_mask[:, :, -1].reshape(loss_mask.size(0), loss_mask.size(1))
             output = self._gpt_forward(
                 None, None, encoder_input, attention_mask, labels, checkpoint_activations_all_layers, speech_mask=speech_mask, input_audio_tokens=input_audio_tokens, input_text_tokens=input_text_tokens, speech_encoder_emb=speech_encoder_emb, speaker_encoder_emb=speaker_encoder_emb,
             )
