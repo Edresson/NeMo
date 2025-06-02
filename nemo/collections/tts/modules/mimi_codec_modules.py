0fa9d4b6516d15a1be6e3103efb92f10d1c14137
Edresson Casanova <edresson1@gmail.com>
2025-06-02 14:35:31 -0700

    Add CONVNEXT decoder posnet

--- nemo/collections/tts/modules/mimi_codec_modules.py
+++ nemo/collections/tts/modules/mimi_codec_modules.py
@@ -9,6 +9,66 @@ from transformers.models.mimi.modeling_mimi import MimiEncoder, MimiTransformerM
 from nemo.core.classes.module import NeuralModule
 from nemo.collections.tts.parts.utils.helpers import get_mask_from_lengths
 
+
+
+
+
+
+from typing import Optional, Tuple
+
+import torch
+from torch import nn
+from torch.nn.utils import weight_norm, remove_weight_norm
+from nemo.collections.asr.parts.submodules.causal_convs import CausalConv1D
+
+
+class ConvNeXtBlock(nn.Module):
+    """ConvNeXt Block adapted from https://github.com/facebookresearch/ConvNeXt to 1D audio signal.
+
+    Args:
+        dim (int): Number of input channels.
+        intermediate_dim (int): Dimensionality of the intermediate layer.
+        layer_scale_init_value (float, optional): Initial value for the layer scale. None means no scaling.
+            Defaults to None.
+        adanorm_num_embeddings (int, optional): Number of embeddings for AdaLayerNorm.
+            None means non-conditional LayerNorm. Defaults to None.
+    """
+
+    def __init__(
+        self,
+        dim: int,
+        intermediate_dim: int = 1152,
+        layer_scale_init_value: float = 1.0,
+    ):
+        super().__init__()
+        self.dwconv = nn.Conv1d(dim, dim, kernel_size=7, padding=3, groups=dim)  # depthwise conv
+        # self.dwconv = CausalConv1D(dim, dim, kernel_size=7, padding=None, groups=dim)  # depthwise conv
+        self.norm = nn.LayerNorm(dim, eps=1e-6)
+        self.pwconv1 = nn.Linear(dim, intermediate_dim)  # pointwise/1x1 convs, implemented with linear layers
+        self.act = nn.GELU()
+        self.pwconv2 = nn.Linear(intermediate_dim, dim)
+        self.gamma = (
+            nn.Parameter(layer_scale_init_value * torch.ones(dim), requires_grad=True)
+            if layer_scale_init_value > 0
+            else None
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        residual = x
+        x = self.dwconv(x)
+        x = x.transpose(1, 2)  # (B, C, T) -> (B, T, C)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+        x = x.transpose(1, 2)  # (B, T, C) -> (B, C, T)
+
+        x = residual + x
+        return x
+
+
 from contextlib import contextmanager
 @contextmanager
 def default_precision(dtype=torch.float32):
@@ -108,12 +168,15 @@ class ReshapeTransformerDecoder(NeuralModule):
         max_position_embeddings: int = 8000,
         rope_theta: float = 10000.0,
         attn_implementation: str = "eager",
+        use_conv_pos: bool = False,
+        num_pos_conv_blocks: int = 1,
     ):
         super().__init__()
 
         self.samples_per_frame = samples_per_frame
         self.audio_proj_size = audio_proj_size
         self.is_causal = is_causal
+        self.use_conv_pos = use_conv_pos
 
         self.config = MimiConfig()
         self.config._attn_implementation = attn_implementation
@@ -131,6 +194,18 @@ class ReshapeTransformerDecoder(NeuralModule):
         self.out_projection = nn.Linear(d_model, audio_proj_size)
         self.out_projection_no_bias = nn.Linear(audio_proj_size, samples_per_frame, bias=False)
 
+        # add ConvNeXt based conv pos
+        if self.use_conv_pos:
+            self.conv_pos = nn.ModuleList(
+            [
+                ConvNeXtBlock(
+                    dim=d_model
+                )
+                for _ in range(num_pos_conv_blocks)
+            ]
+        )
+
+
     def forward(self, inputs, input_len):
         if self.is_causal:
             mask = get_mask_from_lengths(input_len)
@@ -141,6 +216,12 @@ class ReshapeTransformerDecoder(NeuralModule):
         encoded_len = input_len
         out = self.inp_projection(inputs.transpose(1, 2))
         out = self.layers(out, attention_mask=mask)[0]
+
+        if self.use_conv_pos:
+            out = out.transpose(1, 2)
+            for conv_block in self.conv_pos:
+                out = conv_block(out)
+            out = out.transpose(1, 2)
 
         out = self.out_projection(out)
         audio = self.out_projection_no_bias(out)
